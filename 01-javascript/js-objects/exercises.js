c624656429b3ced7be869fc700ab0c04a17dffdf
Cold Head <certainthings@gmail.com>
2015-08-06 12:31:09 +1000

    reading list

--- 01-javascript/js-objects/exercises.js
+++ 01-javascript/js-objects/exercises.js
@@ -20,3 +20,45 @@ var recipePrinter = function (recipe) {
 }
 
 recipePrinter(hotdogRecipe);
+
+// The Reading List
+
+// Create an array of objects, where each object describes a book and has properties for the title (a string), author (a string), and alreadyRead (a boolean indicating if you read it yet).
+// Iterate through the array of books. For each book, log the book title and book author like so: "The Hobbit by J.R.R. Tolkien".
+// Now use an if/else statement to change the output depending on whether you read it yet or not. If you read it, log a string like 'You already read "The Hobbit" by J.R.R. Tolkien', and if not, log a string like 'You still need to read "The Lord of the Rings" by J.R.R. Tolkien.'
+
+var readingList = [
+  {title: 'Catch-22', author: 'Joseph Heller', alreadyRead: true},
+  {title: '50 Shades of Grey', author: 'Some Asshole', alreadyRead: false},
+  {title: 'Voss', author: 'Patrick White', alreadyRead: true}
+];
+
+for (var i = 0; i < readingList.length; i++) {
+  var book = readingList[i];
+  if (book.alreadyRead === true) {
+    console.log("You already read " + book.title + ' by ' + book.author);
+  } else {
+    console.log("You still need to read " + book.title + ' by ' + book.author);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
