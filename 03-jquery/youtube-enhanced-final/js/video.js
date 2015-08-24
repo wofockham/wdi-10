c60da673071d301540fc72fac7c6c1b39187ae64
Cold Head <certainthings@gmail.com>
2015-08-24 11:56:10 +1000

    funtext is not actually that much fun

--- 03-jquery/youtube-enhanced-final/js/video.js
+++ 03-jquery/youtube-enhanced-final/js/video.js
@@ -1,44 +1,37 @@
-// Add Youtube thumbnail images to each of the Youtube links on the page.
+$(document).ready(function () {
 
-var thumbnailify = function (link) {
-  var $link = $( link ); // jQuerify the link node.
+  $('h2').funText(500, 'rainbow');
 
-  var video = $link.attr('href');
-  var thumbnailURL = youtube.generateThumbnailUrl(video);
+  // Add Youtube thumbnail images to each of the Youtube links on the page.
 
-  $('<img>').attr('src', thumbnailURL).appendTo($link);
+  var thumbnailify = function (link) {
+    var $link = $( link ); // jQuerify the link node.
 
-  $link.on('click', function (e) {
-    e.preventDefault(); // Don't follow the link, we will play the video ourselves.
+    var video = $link.attr('href');
+    var thumbnailURL = youtube.generateThumbnailUrl(video);
 
-    var embedURL = youtube.generateEmbedUrl(video);
+    $('<img>').attr('src', thumbnailURL).appendTo($link);
 
-    var iframe = $('<iframe width="420" height="315" frameborder="0" allowfullscreen></iframe>');
-    iframe.attr('src', embedURL);
+    $link.on('click', function (e) {
+      e.preventDefault(); // Don't follow the link, we will play the video ourselves.
 
-    $('#video').hide().html(iframe).fadeIn(2000);
+      var embedURL = youtube.generateEmbedUrl(video);
 
+      var iframe = $('<iframe width="420" height="315" frameborder="0" allowfullscreen></iframe>');
+      iframe.attr('src', embedURL);
+
+      $('#video').hide().html(iframe).fadeIn(2000);
+
+    });
+  };
+
+  var $links = $('a');
+
+  // Add a thumbnail to each link.
+  $links.each(function () {
+    thumbnailify(this);
   });
-};
 
-var $links = $('a');
+  debugger;
 
-// Add a thumbnail to each link.
-$links.each(function () {
-  thumbnailify(this);
 });
-
-// for (var i = 0; i < $links.length; i++) {
-
-//   var $link = $links.eq(i);
-//   var video = $link.attr('href');
-
-//   var thumbnailURL = youtube.generateThumbnailUrl(video);
-
-//   var $thumbnail = $('<img>');
-//   $thumbnail.attr('src', thumbnailURL);
-
-//   $link.append($thumbnail);
-
-// }
-
