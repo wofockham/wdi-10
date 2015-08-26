0f3ece062fbcd9f4167f110780d49836a9676951
Cold Head <certainthings@gmail.com>
2015-08-26 12:59:08 +1000

    calc demo

---
+++ 05-sinatra/calc/main.rb
@@ -0,0 +1,41 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'pry'
+
+get '/' do
+  erb :calc
+end
+
+get '/calc' do
+  @x = params[:x].to_f
+  @y = params[:y].to_f
+
+  @result = case params[:operation]
+  when '+' then @x + @y
+  when '-' then @x - @y
+  when '*' then @x * @y
+  when '/' then @x / @y
+  end
+
+  erb :calc
+end
+
+get '/about' do
+  erb :about
+end
+
+get '/help' do
+  erb :help
+end
+
+get '/advertise' do
+  erb :advertise
+end
+
+
+
+
+
+
+
+
