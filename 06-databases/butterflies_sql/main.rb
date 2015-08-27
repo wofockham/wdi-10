a01af75054fe06b6f4cf9266d2dbb452e3d79389
Cold Head <certainthings@gmail.com>
2015-08-27 16:14:53 +1000

    thank god that's over

--- 06-databases/butterflies_sql/main.rb
+++ 06-databases/butterflies_sql/main.rb
@@ -15,7 +15,7 @@ get '/butterflies/new' do
   erb :'butterflies/new'
 end
 
-post '/butterflies/create' do
+post '/butterflies' do
   query_db "INSERT INTO butterflies (name, family, image) VALUES ('#{params['name']}', '#{params['family']}', '#{params['image']}')"
   redirect to '/butterflies'
 end
@@ -25,6 +25,20 @@ get '/butterflies/:id' do
   erb :'butterflies/show'
 end
 
+get '/butterflies/:id/delete' do
+  query_db "DELETE FROM butterflies WHERE id = #{ params[:id] }"
+  redirect to '/butterflies'
+end
+
+get '/butterflies/:id/edit' do
+  @butterfly = query_db("SELECT * FROM butterflies WHERE id = #{ params[:id] }").first
+  erb :'butterflies/edit'
+end
+
+post '/butterflies/:id' do
+  query_db "UPDATE butterflies SET name='#{ params['name'] }', family='#{ params['family']}', image='#{ params['image'] }' WHERE id = #{ params['id'] }"
+  redirect to "/butterflies/#{ params[:id] }"
+end
 
 
 def query_db(sql)
