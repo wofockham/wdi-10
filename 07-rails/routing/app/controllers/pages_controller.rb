b6974096b7ce71ca6ed162939b682fbc23c3cfa3
Cold Head <certainthings@gmail.com>
2015-08-31 15:13:01 +1000

    routing demos

--- 07-rails/routing/app/controllers/pages_controller.rb
+++ 07-rails/routing/app/controllers/pages_controller.rb
@@ -7,4 +7,7 @@ class PagesController < ApplicationController
 
   def about
   end
+
+  def lol
+  end
 end
--- 07-rails/routing/config/routes.rb
+++ 07-rails/routing/config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   get '/home' => 'pages#home'
   get '/faq' => 'pages#faq'
   get '/about' => 'pages#about'
+  get '/funny' => 'pages#lol'
 
   get '/auto/:color' => 'auto#color'
   get '/auto/:hp/:torque' => 'auto#engine'
+
+  get '/calc/:x/:y/:operator' => 'calc#calculate'
 end
