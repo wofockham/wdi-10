373630045da6f50eec0e0f082d6aa863f4e759b4
Cold Head <certainthings@gmail.com>
2015-09-01 10:52:10 +1000

    number helpers demo

---
+++ 07-rails/helpers/app/controllers/pages_controller.rb
@@ -0,0 +1,20 @@
+class PagesController < ApplicationController
+
+  def home
+  end
+
+  def numbers
+    @amount = 234
+    @large_number = 24802340349
+    @phone = 2125551212
+  end
+
+  def text
+  end
+
+  def assets
+  end
+
+  def url
+  end
+end
