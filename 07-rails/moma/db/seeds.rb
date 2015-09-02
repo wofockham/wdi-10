f86459564d69c8f9890d1b0484af57906263f358
Cold Head <certainthings@gmail.com>
2015-09-02 10:53:08 +1000

    artist index

---
+++ 07-rails/moma/db/seeds.rb
@@ -0,0 +1,3 @@
+Artist.destroy_all
+
+Artist.create(:name => 'Joan Miro', :nationality => 'Spanish', :dob => '1893/04/20', :period => '20th century', :image => 'http://upload.wikimedia.org/wikipedia/commons/5/5c/Portrait_of_Joan_Miro%2C_Barcelona_1935_June_13.jpg')
