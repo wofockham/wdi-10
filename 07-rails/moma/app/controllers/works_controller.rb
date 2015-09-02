6768ca7b214c93d56fb30306a834d6ef2e7cfbaf
Cold Head <certainthings@gmail.com>
2015-09-02 15:05:28 +1000

    associations part i

--- 07-rails/moma/app/controllers/works_controller.rb
+++ 07-rails/moma/app/controllers/works_controller.rb
@@ -8,14 +8,32 @@ class WorksController < ApplicationController
   end
 
   def new
+    @work = Work.new
+  end
+
+  def create
+    work = Work.create work_params
+    redirect_to work
   end
 
   def edit
+    @work = Work.find params[:id]
+  end
+
+  def update
+    work = Work.find params[:id]
+    work.update work_params
+    redirect_to work
   end
 
   def destroy
     work = Work.find params[:id]
     work.destroy
     redirect_to works_path
+  end
+
+  private
+  def work_params
+    params.require(:work).permit(:title, :year, :medium, :style, :image, :artist_id)
   end
 end
--- 07-rails/moma/db/schema.rb
+++ 07-rails/moma/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150902022715) do
+ActiveRecord::Schema.define(version: 20150902044411) do
 
   create_table "artists", force: :cascade do |t|
     t.text     "name"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20150902022715) do
     t.text     "image"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "artist_id"
   end
 
 end
