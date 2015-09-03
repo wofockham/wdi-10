745c0d5bea3ed155b12a572733293d4450bc3370
Cold Head <certainthings@gmail.com>
2015-09-03 13:04:02 +1000

    helper demos

--- 07-rails/helpers/app/controllers/pages_controller.rb
+++ 07-rails/helpers/app/controllers/pages_controller.rb
@@ -10,6 +10,10 @@ class PagesController < ApplicationController
   end
 
   def text
+    @numbers = (1..30).to_a
+    @error_count1 = 1
+    @error_count2 = 23
+    @story = 'A long long time ago in a galaxy far far away'
   end
 
   def assets
--- 07-rails/helpers/config/routes.rb
+++ 07-rails/helpers/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root :to => 'pages#home'
 
   get '/numbers' => 'pages#numbers'
-  get '/text' => 'pages#text'
+  get '/client_is_an_idiot' => 'pages#text', :as => 'text'
   get '/assets' => 'pages#assets' # !!!
   get '/url' => 'pages#url'
 
