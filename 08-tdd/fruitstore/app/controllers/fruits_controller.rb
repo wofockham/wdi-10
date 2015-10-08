3933665118969619c0099934ff8a5cccc01d46b9
Cold Head <certainthings@gmail.com>
2015-10-08 12:35:56 +1100

    fruits#index

--- 08-tdd/fruitstore/app/controllers/fruits_controller.rb
+++ 08-tdd/fruitstore/app/controllers/fruits_controller.rb
@@ -1,2 +1,10 @@
 class FruitsController < ApplicationController
+  def index
+    @fruits = Fruit.all.reverse
+
+    respond_to do |format|
+      format.html {}
+      format.json { render :json => @fruits }
+    end
+  end
 end
